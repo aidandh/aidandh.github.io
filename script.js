4e2548137f63c7048d9014e61a8f2c20a8bd5dff
Aidan Hert <aidanhert@hotmail.com>
2024-08-25 00:03:02 -0700

    added prefers-color-scheme check

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let lightMode = true;
-const lightModeSelectorContainer = document.getElementById('light-mode-selector-container');
-lightModeSelectorContainer.addEventListener('click', () => {
+let lightMode;
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    lightMode = false;
+} else {
+    lightMode = true;
+}
+
+function changeLightMode() {
     const styledElements = [...document.getElementsByClassName(lightMode ? 'light' : 'dark')];
     for (const element of styledElements) {
         element.classList.remove(lightMode ? 'light' : 'dark');
         element.classList.add(lightMode ? 'dark' : 'light');
     }
     lightMode = !lightMode;
-});
+}
+
+changeLightMode();
+
+const lightModeSelectorContainer = document.getElementById('light-mode-selector-container');
+lightModeSelectorContainer.addEventListener('click', changeLightMode);
