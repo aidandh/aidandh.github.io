6fe59bc376c8b17b802158979a9ed5b08b12d214
Aidan Hert <aidanhert@hotmail.com>
2025-07-22 22:24:18 -0700

    prevent flickering

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function checkLightModeUserPreference() {
-    const savedMode = localStorage.getItem('preferredMode');
-    const browserPefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const browserPrefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
-    const browserMode = browserPefersDark ? 'dark' : browserPrefersLight ? 'light' : undefined;
-    const mode = savedMode || browserMode || 'light';
-    const root = document.documentElement;
-    if (mode === 'dark') {
-        root.classList.add('dark');
-        localStorage.setItem('preferredMode', 'dark');
-    } else {
-        root.classList.add('light');
-        localStorage.setItem('preferredMode', 'light');
-    }
-}
-
 function checkLightMode() {
     const root = document.documentElement;
     return root.classList.contains('light');
@@ -26,8 +10,6 @@ function changeLightMode() {
     root.classList.remove(isLightMode ? 'light': 'dark');
     localStorage.setItem('preferredMode', isLightMode ? 'dark': 'light');
 }
-
-checkLightModeUserPreference();
 
 const lightModeSelectorContainer = document.getElementById('light-mode-selector-container');
 lightModeSelectorContainer.addEventListener('click', changeLightMode);
