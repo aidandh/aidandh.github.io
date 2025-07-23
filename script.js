1cdc506eaf6c64d6ddabfc6d53d615ee9940a261
Aidan Hert <aidanhert@hotmail.com>
2025-07-22 22:20:59 -0700

    light mode improvements

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let lightMode = true;
-
 function checkLightModeUserPreference() {
-    const userPrefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-    if (userPrefersDarkMode) {
-        changeLightMode();
+    const savedMode = localStorage.getItem('preferredMode');
+    const browserPefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const browserPrefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+    const browserMode = browserPefersDark ? 'dark' : browserPrefersLight ? 'light' : undefined;
+    const mode = savedMode || browserMode || 'light';
+    const root = document.documentElement;
+    if (mode === 'dark') {
+        root.classList.add('dark');
+        localStorage.setItem('preferredMode', 'dark');
+    } else {
+        root.classList.add('light');
+        localStorage.setItem('preferredMode', 'light');
     }
 }
 
+function checkLightMode() {
+    const root = document.documentElement;
+    return root.classList.contains('light');
+}
+
 function changeLightMode() {
-    const styledElements = [...document.getElementsByClassName(lightMode ? 'light' : 'dark'), document.body];
-    for (const element of styledElements) {
-        element.classList.remove(lightMode ? 'light' : 'dark');
-        element.classList.add(lightMode ? 'dark' : 'light');
-    }
-    lightMode = !lightMode;
+    const root = document.documentElement;
+    const isLightMode = checkLightMode();
+    root.classList.add(isLightMode ? 'dark': 'light');
+    root.classList.remove(isLightMode ? 'light': 'dark');
+    localStorage.setItem('preferredMode', isLightMode ? 'dark': 'light');
 }
 
 checkLightModeUserPreference();
